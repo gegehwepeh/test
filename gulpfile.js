7dedca0b40c789b47272ec33728428dbc3b06166
Armix <sepatuan2@gmail.com>
2015-12-09 15:28:22 +0700

    add tab to info and give example of menu at home

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,6 @@ gulp.task('vendor', function() {
   return gulp.src([
     'public/js/jquery.js',
     'public/js/bootstrap.min.js',
-    'public/js/evenfly.js',
     'public/js/jquery.nicescroll.min.js',
     'public/js/jquery.easing.min.js',
     'public/js/jquery.fittext.js',
